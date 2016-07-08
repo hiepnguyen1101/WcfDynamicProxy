a0854f78bc343184b6f9d13e1a215b5858e38878
hoangnl <nguyenlehoang@hotmail.com>
2016-07-08 20:05:42 +0700

    Bổ sung project test
    Bổ sung project test

---
+++ NorthwindTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using NorthwindContracts.DataContracts;
+using NUnit.Framework;
+
+namespace NorthwindTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+            var category = new Category();
+            category.CategoryID = 1;
+            Assert.AreEqual(1, category.CategoryID);
+        }
+    }
+}
